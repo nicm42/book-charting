44a1cfdf5f14d40d898e91a4bdfa54ce5fb2d270
“nicm42” <42087659+nicm42@users.noreply.github.com>
2024-01-22 19:42:50 +0000

    Changed to load a json file

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,9 @@
 import './style.css';
+import data from '../../data.json';
 
 const getData = async () => {
-  const response = await fetch('/api');
-  const data = await response.json();
+  /* const response = await fetch('/api');
+  const data = await response.json(); */
   console.log(data);
 };
 
