b82fe92fbf0c8e700b0e34ecb3353f3e69f693dd
“nicm42” <42087659+nicm42@users.noreply.github.com>
2024-02-03 20:46:23 +0000

    Added loading and error messages

--- src/app/getData.ts
+++ src/app/getData.ts
@@ -1,12 +1,34 @@
 import calculateNumbers from './calculateNumbers';
 import IDataToSend from '../interfaces/IDataToSend';
-import data from '../../data.json';
+//import data from '../../data.json';
 
 const getData = async () => {
-  /* const response = await fetch('/api');
-  const data: IDataToSend = await response.json(); */
-  console.log(data);
-  calculateNumbers(data);
+  const loading: HTMLElement | null = document.querySelector('.loading');
+  const error: HTMLParagraphElement | null = document.querySelector('.error');
+  const chartHeadings: NodeListOf<HTMLHeadingElement> =
+    document.querySelectorAll('.chart-heading');
+
+  const response = await fetch('/api');
+  if (response.ok) {
+    const data: IDataToSend[] = await response.json();
+    //console.log(data);
+
+    if (loading) {
+      loading.style.display = 'none';
+    }
+
+    if (chartHeadings) {
+      chartHeadings.forEach(
+        (chartHeading) => (chartHeading.style.display = 'block')
+      );
+    }
+
+    calculateNumbers(data);
+  } else {
+    if (error) {
+      error.style.display = 'block';
+    }
+  }
 };
 
 export default getData;
